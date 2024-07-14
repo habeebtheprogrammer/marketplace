d0fc9b545cca57421cf0cd4693878dc06ae156b8
habeeb <habibmail31@yahoo.com>
2024-07-14 23:41:39 +0100

    more fixes

--- utils/responder.js
+++ utils/responder.js
@@ -1,11 +1,10 @@
 const { errMesg, requiredErr } = require('./constant');
 const { HttpStatusCode } = require('axios');
 
-const ApiResponder = (res, statusCode, message, payload, extra = {}) => {
+const ApiResponder = (res, statusCode, payload, extra = {}) => {
   res.status(statusCode).send({
     status: statusCode,
     success: statusCode === HttpStatusCode.Ok || statusCode === HttpStatusCode.Created ? true : false,
-    message,
     data: payload,
     ...extra,
   });
@@ -17,19 +16,17 @@ exports.successResponse = (res, payload = {}, message = 'Success', statusCode = 
 
 exports.errorResponse = (res,  error, message = errMesg, statusCode = HttpStatusCode.InternalServerError) => {
     var errorData  = []
-    console.log(error)
     if(error?.name == 'ValidationError'){
-        Object.keys(error?.errors).forEach(i => errorData.push({[i] : error.errors[i].message}))
-        message = requiredErr
+        Object.keys(error?.errors).forEach(i => errorData.push(error.errors[i].message))
     } else if(error?.code == "11000" ){
         Object.keys(error.keyValue).forEach(i => {
             message = error.keyValue[i]
-            errorData.push({[i] : error.keyValue[i] + " is not available"})
+            errorData.push(error.keyValue[i] + " is not available")
         })
-        message +=  " is not available"
     } else if(error?.message && error?.name != 'JsonWebTokenError'){
         message = error.message
+        errorData.push(error.message)
     }  
-    return ApiResponder(res, statusCode, message, {errors: errorData});
+    return ApiResponder(res, statusCode,  {errors: errorData});
 };
   
