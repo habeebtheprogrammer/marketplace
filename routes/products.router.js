b773e57ce16e14bbc9212fded2eec3e839bbf733
habeeb <habibmail31@yahoo.com>
2024-07-14 18:13:19 +0100

    product route now public

--- routes/products.router.js
+++ routes/products.router.js
@@ -4,7 +4,7 @@ const { checkAuth, vendorsAccessOnly } = require('../utils/auth');
 const { productCreationValidator, updateValidator } = require('../utils/validator');
 var router = express.Router();
 
-router.get('/', checkAuth, productsController.getProducts);
+router.get('/', productsController.getProducts);
 router.post('/', checkAuth, vendorsAccessOnly, productCreationValidator, productsController.createProducts);
 router.patch('/', checkAuth, vendorsAccessOnly, updateValidator, productsController.updateProducts);
 
