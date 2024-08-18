277ac72bd5b4c518d09d8e322b4e82e7f53b5663
habeeb <habibmail31@yahoo.com>
2024-08-18 13:10:47 +0100

    more fixes

--- controller/swap.controller.js
+++ controller/swap.controller.js
@@ -1,10 +1,12 @@
 const { swapService } = require("../service")
+const { sendSwapEmail } = require("../utils/helpers")
 const { successResponse, errorResponse } = require("../utils/responder")
 
 exports.createSwapDetails = async (req, res, next) => {
     try {
         const data = await swapService.createSwap({...req.body, userId: req.userId})
         successResponse(res, data)
+        sendSwapEmail(req.body)
     } catch (error) {
         errorResponse(res, error)
     }
