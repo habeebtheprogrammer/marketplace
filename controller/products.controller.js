28606c0dee7d76a7bf7157f45a409819206527bb
habeeb <habibmail31@yahoo.com>
2024-08-23 21:37:22 +0100

    more fixes

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -39,6 +39,7 @@ exports.getProducts = async (req, res, next) => {
         var { sort, limit = 9, page = 1 } = req.query
 
         var pagination = { limit, page }
+        console.log(querystring.stringify(req.query))
         const query = buildFilterQuery(querystring.stringify(req.query));
         console.log(JSON.stringify(query),limit)
         sort = sort == 'highToLow' ? { sort: { original_price: -1 } } : sort == 'lowToHigh' ? { sort: { rating: -1 } } : {};
