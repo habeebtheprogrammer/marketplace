4ea9c61f432845014702af77d30b5667ce800385
habeebtheprogrammer <habibmail31@yahoo.com>
2025-03-14 08:27:56 +0100

    more fixes

--- test.js
+++ test.js
@@ -1665,6 +1665,7 @@ const filters = [
     { vendor: 'quickvtu', network: 'AIRTEL', planToRemove: 'GIFTING' },
     { vendor: 'quickvtu', network: 'MTN', planToRemove: 'COOPERATE GIFTING' },
     { vendor: 'quickvtu', network: 'MTN', planToRemove: 'SME' },
+    { vendor: 'bilal', network: 'MTN', planToRemove: 'SME' },
     { vendor: 'bilal', network: 'MTN', planToRemove: 'GIFTING' },
     { vendor: 'bilal', network: 'MTN', planToRemove: 'COOPERATE GIFTING' },
     // { vendor: 'bilal', network: 'AIRTEL', planToRemove: 'GIFTING PROMO' },
