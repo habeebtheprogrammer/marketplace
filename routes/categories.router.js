6870ba8fe11472e7bb4fea81061141e276474925
musamusakannike <musamusakannike@gmail.com>
2025-05-31 00:57:13 +0100

    Added auth middleware to allow both admin and vendors to access a category route

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,11 +1,11 @@
 var express = require('express');
 const { categoriesController } = require('../controller');
-const { checkAuth, vendorsAccessOnly } = require('../utils/authMiddleware');
+const { checkAuth, vendorsAccessOnly, adminOrVendorAccessOnly } = require('../utils/authMiddleware');
 const { categoryCreationValidator, updateValidator } = require('../utils/validator');
 var router = express.Router();
 
 router.get('/', categoriesController.getCategories);
-router.post('/', checkAuth, vendorsAccessOnly, categoryCreationValidator, categoriesController.createCategories);
+router.post('/', checkAuth, adminOrVendorAccessOnly, categoryCreationValidator, categoriesController.createCategories);
 router.patch('/', checkAuth, vendorsAccessOnly, updateValidator, categoriesController.updateCategories);
 router.delete('/:id', checkAuth, vendorsAccessOnly, categoriesController.deleteCategory);
 
