c048f111c80fa3e3b94b765fbcfb230450159fff
habeebtheprogrammer <habibmail31@yahoo.com>
2025-08-05 18:03:50 +0100

    more fixes

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -88,7 +88,7 @@ exports.getProducts = async (req, res, next) => {
         : sort == "lowToHigh"
         ? { sort: { ...searchSortObj, original_price: 1 , videoUrl: 1} }
         : sort == "latest" ?   { sort: { videoUrl: -1} } : 
-        { sort: { ...searchSortObj,  videoUrl: -1 } };
+        { sort: { ...searchSortObj,  videoUrl: -1 , _id: -1} };
 
     if (title) sort.sort.score = { $meta: "textScore" };
     const options = {
